f28cc06c1b78811f8b221e9937035a6e87b9a772
ichera <ichera087@gmail.com>
2025-10-13 21:10:41 +0300

    first commit

---
+++ src/views/HomePage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import AppHeader from '@/components/layouts/AppHeader.vue'
+</script>
+
+<template>
+  <AppHeader />
+</template>
+
+<style scoped></style>
---
+++ src/views/NotFoundPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>проеб ошибка 404</h1>
+</template>
+
+<style scoped></style>
